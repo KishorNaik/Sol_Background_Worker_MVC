7aa9d9210f92d97966080dab3f07c739fb5c24f0
Kishor Naik <kishor.naik011.net@outlook.com>
2019-10-17 13:38:58 +0530

    Code Commit

---
+++ Sol_Background_Worker/Sol_Background_Worker/Controllers/DemoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DalSoft.Hosting.BackgroundQueue;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sol_Background_Worker.Controllers
+{
+    public class DemoController : Controller
+    {
+        private readonly BackgroundQueue backgroundQueue;
+
+        public DemoController(BackgroundQueue backgroundQueue)
+        {
+            this.backgroundQueue = backgroundQueue;
+        }
+
+        #region Private Method
+
+        private async Task LongRunningTask()
+        {
+            await Task.Run(async () =>
+            {
+                for (int counter = 0; counter <= 100; counter++)
+                {
+                    await Task.Delay(1000);
+                }
+            });
+        }
+
+        #endregion Private Method
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult OnPost()
+        {
+            backgroundQueue.Enqueue(async (cancelToken) =>
+            {
+                await LongRunningTask();
+            });
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Sol_Background_Worker/Sol_Background_Worker/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DalSoft.Hosting.BackgroundQueue.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sol_Background_Worker
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddBackgroundQueue(maxConcurrentCount: 1, millisecondsToWaitBeforePickingUpTask: 1000, onException: (leexception) => { });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
